6e689cecc7fbbaf5a8be0e14c9eb784329d63a80
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-10-22 11:00:48 +0100

    013 - Intro Alert - Second click event

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
         $('#introText').css('margin', '20px 5px');
         $('#intro').css('top', '35vh');
         $('#introText').html('In this game, three numbers are randomly selected to form a combination, which you have to guess using the given text boxes.');
-
         
+        // Intro Alert - Second click event
+        $('#introBtn').click(function () {
+            $('#intro').css('top', '40vh');
+            $('#introText').html('The numbers will be between 1 and 99. When you have made your guess, click on the "Submit Answer" button.');
+
+            
+        });
     });
 });
