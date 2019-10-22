4784635617842240eec57f17ecffb6eba3c12bd4
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-10-22 11:07:58 +0100

    014 - Intro Alert - Third click event

--- main.js
+++ main.js
@@ -9,8 +9,15 @@ $(document).ready(function() {
         $('#introBtn').click(function () {
             $('#intro').css('top', '40vh');
             $('#introText').html('The numbers will be between 1 and 99. When you have made your guess, click on the "Submit Answer" button.');
-
-            
+        
+            // Intro Alert - Third click event
+            $('#introBtn').click(function() {
+                $('#intro').css('top', '35vh');
+                $('#introText').css('margin', '20px 10px');
+                $('#introText').html("If any of the numbers you've entered is incorrect, the page will tell you which number is incorrect, by telling you whether the number selected is higher/lower than the number you've entered.");
+                
+                
+            });
         });
     });
 });
