1c17d847b0a95f89e728db8324771083f09be6af
Mahdi Uddin <mahdiuddin256@gmail.com>
2019-10-24 14:23:07 +0100

    033 - Game Logic - Game Over

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 // ------------------------------- Variables ------------------------------- //
-let r1 = Math.floor((Math.random() * 98) + 1);
-let r2 = Math.floor((Math.random() * 98) + 1);
-let r3 = Math.floor((Math.random() * 98) + 1);
+let r1 = Math.floor(Math.random() * 98 + 1);
+let r2 = Math.floor(Math.random() * 98 + 1);
+let r3 = Math.floor(Math.random() * 98 + 1);
 
-let boxArr = [$('#boxA'), $('#boxB'), $('#boxC')];
+let boxArr = [
+	$('#boxA'),
+	$('#boxB'),
+	$('#boxC')
+];
 
-let comboArr = [r1, r2, r3];
+let comboArr = [
+	r1,
+	r2,
+	r3
+];
 
 // ------------------------------- Functions ------------------------------- //
 // ---------------- Intro Section ---------------- //
 function intro1() {
-    $('#introText').css('margin', '20px 5px');
+	$('#introText').css('margin', '20px 5px');
 	$('#intro').css('top', '35vh');
 	$('#introText').html(
 		'In this game, three numbers are randomly selected to form a combination, which you have to guess by typing your guesses into the given boxes.'
@@ -21,126 +29,183 @@ function intro1() {
 }
 
 function intro2() {
-    $('#intro').css('top', '35vh');
-    $('#introText').html(
-        'The numbers will be between 1 and 99. When you have made your guess, click on the "Submit Answer" button.'
-        );
+	$('#intro').css('top', '35vh');
+	$('#introText').html(
+		'The numbers will be between 1 and 99. When you have made your guess, click on the "Submit Answer" button.'
+	);
 
-    // Intro Alert - Third click event
-    $('#introBtn').click(intro3);
+	// Intro Alert - Third click event
+	$('#introBtn').click(intro3);
 }
 
 function intro3() {
-    $('#intro').css('top', '35vh');
-    $('#introText').css('margin', '20px 10px');
-    $('#introText').html(
-        "If any of the numbers you've entered is incorrect, the page will tell you which number is incorrect, by telling you whether the number selected is higher/lower than the number you've entered."
-    );
+	$('#intro').css('top', '35vh');
+	$('#introText').css('margin', '20px 10px');
+	$('#introText').html(
+		"If any of the numbers you've entered is incorrect, the page will tell you which number is incorrect, by telling you whether the number selected is higher/lower than the number you've entered."
+	);
 
-    // Intro Alert - Fourth click event
-    $('#introBtn').click(intro4);
+	// Intro Alert - Fourth click event
+	$('#introBtn').click(intro4);
 }
 
 function intro4() {
-    $('#intro').css('top', '40vh');
-    $('#introBtn').css('display', 'none');
-    $('#introText').html(
-        'To start playing, click on the white X on the top-right corner of the page.'
-    );
+	$('#intro').css('top', '40vh');
+	$('#introBtn').css('display', 'none');
+	$('#introText').html(
+		'To start playing, click on the white X on the top-right corner of the page.'
+	);
 }
 
 // ---------------- Close Intro ---------------- //
 function closeBtn() {
-    $('#alertContainer').css('display', 'none');
-    $('#container').css('display', 'block');
-    
-    comboGen();
+	$('#alertContainer').css('display', 'none');
+	$('#container').css('display', 'block');
+
+	comboGen();
 }
 
 // ---------------- Game Logic - Combination Generator ---------------- //
 
 function comboGen() {
-    r1 = Math.floor((Math.random() * 98) + 1);
-    r2 = Math.floor((Math.random() * 98) + 1);
-    r3 = Math.floor((Math.random() * 98) + 1);
+	r1 = Math.floor(Math.random() * 98 + 1);
+	r2 = Math.floor(Math.random() * 98 + 1);
+	r3 = Math.floor(Math.random() * 98 + 1);
 
-    if (r1 == r2 && r1 == r3 || r1 == r2 || r1 == r3 || r2 == r3) {
-        r1 = Math.floor((Math.random() * 98) + 1);
-        r2 = Math.floor((Math.random() * 98) + 1);
-        r3 = Math.floor((Math.random() * 98) + 1);
-    }
+	if ((r1 == r2 && r1 == r3) || r1 == r2 || r1 == r3 || r2 == r3) {
+		r1 = Math.floor(Math.random() * 98 + 1);
+		r2 = Math.floor(Math.random() * 98 + 1);
+		r3 = Math.floor(Math.random() * 98 + 1);
+	}
 
-    let combo = r1 + '-' + r2 + '-' + r3;
-    comboArr = [r1, r2, r3];
+	let combo = r1 + '-' + r2 + '-' + r3;
+	comboArr = [
+		r1,
+		r2,
+		r3
+	];
 
-    console.log(r1, r2, r3, combo, comboArr);
+	console.log(r1, r2, r3, combo, comboArr);
 
-    $('#combo').html(combo);
+	$('#combo').html(combo);
 
-    $('#game').submit(checkAns);
+	$('#game').submit(checkAns);
 }
 
 // ---------------- Game Logic - Combination Checker ---------------- //
 
 function checkAns(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    // #boxA's input checker
-    if (boxArr[0][0].value == comboArr[0]) {
-        $('#lowA').css('display', 'none');
-        $('#highA').css('display', 'block');
-        $('#highA').html('Correct!');
-    } else if (boxArr[0][0].value >= comboArr[0]) {
-        $('#lowA').css('display', 'none');
-        $('#highA').css('display', 'block');
-        $('#highA').html('High');
-    } else if (boxArr[0][0].value <= comboArr[0]) {
-        $('#highA').css('display', 'none');
-        $('#lowA').css('display', 'block');
-    } else {
-        $('#lowA').css('display', 'none');
-        $('#highA').css('display', 'none');
-    }
+	// #boxA's input checker
+	if (boxArr[0][0].value == comboArr[0]) {
+		$('#lowA').css('display', 'none');
+		$('#highA').css('display', 'block');
+		$('#highA').html('Correct!');
+	} else if (boxArr[0][0].value >= comboArr[0]) {
+		$('#lowA').css('display', 'none');
+		$('#highA').css('display', 'block');
+		$('#highA').html('High');
+	} else if (boxArr[0][0].value <= comboArr[0]) {
+		$('#highA').css('display', 'none');
+		$('#lowA').css('display', 'block');
+	} else {
+		$('#lowA').css('display', 'none');
+		$('#highA').css('display', 'none');
+	}
 
-    // #boxB's input checker
-    if (boxArr[1][0].value == comboArr[1]) {
-        $('#lowB').css('display', 'none');
-        $('#highB').css('display', 'block');
-        $('#highB').html('Correct!');
-    } else if (boxArr[1][0].value >= comboArr[1]) {
-        $('#lowB').css('display', 'none');
-        $('#highB').css('display', 'block');
-        $('#highB').html('High');
-    } else if (boxArr[1][0].value <= comboArr[1]) {
-        $('#highB').css('display', 'none');
-        $('#lowB').css('display', 'block');
-    } else {
-        $('#lowB').css('display', 'none');
-        $('#highB').css('display', 'none');
-    }
-    
-    // #boxC's input checker
-    if (boxArr[2][0].value == comboArr[2]) {
-        $('#lowC').css('display', 'none');
-        $('#highC').css('display', 'block');
-        $('#highC').html('Correct!');
-    } else if (boxArr[2][0].value >= comboArr[2]) {
-        $('#lowC').css('display', 'none');
-        $('#highC').css('display', 'block');
-        $('#highC').html('High');
-    } else if (boxArr[2][0].value <= comboArr[2]) {
-        $('#highC').css('display', 'none');
-        $('#lowC').css('display', 'block');
-    } else {
-        $('#lowC').css('display', 'none');
-        $('#highC').css('display', 'none');
-    }
+	// #boxB's input checker
+	if (boxArr[1][0].value == comboArr[1]) {
+		$('#lowB').css('display', 'none');
+		$('#highB').css('display', 'block');
+		$('#highB').html('Correct!');
+	} else if (boxArr[1][0].value >= comboArr[1]) {
+		$('#lowB').css('display', 'none');
+		$('#highB').css('display', 'block');
+		$('#highB').html('High');
+	} else if (boxArr[1][0].value <= comboArr[1]) {
+		$('#highB').css('display', 'none');
+		$('#lowB').css('display', 'block');
+	} else {
+		$('#lowB').css('display', 'none');
+		$('#highB').css('display', 'none');
+	}
+
+	// #boxC's input checker
+	if (boxArr[2][0].value == comboArr[2]) {
+		$('#lowC').css('display', 'none');
+		$('#highC').css('display', 'block');
+		$('#highC').html('Correct!');
+	} else if (boxArr[2][0].value >= comboArr[2]) {
+		$('#lowC').css('display', 'none');
+		$('#highC').css('display', 'block');
+		$('#highC').html('High');
+	} else if (boxArr[2][0].value <= comboArr[2]) {
+		$('#highC').css('display', 'none');
+		$('#lowC').css('display', 'block');
+	} else {
+		$('#lowC').css('display', 'none');
+		$('#highC').css('display', 'none');
+	}
+
+	// Game Over
+	if (
+		boxArr[0][0].value == comboArr[0] &&
+		boxArr[1][0].value == comboArr[1] &&
+		boxArr[2][0].value == comboArr[2]
+	) {
+		gameOver();
+	}
+}
+
+// ---------------- Game Logic - Game Over ---------------- //
+
+function gameOver() {
+	// Reveal the combination
+	$('#combination').css('display', 'block');
+
+	// Stop displaying the game
+	$('#game').css('display', 'none');
+
+	// Show some 'Game Over' text
+	$('<span id="gameOver"></span>').insertBefore($('#combination'));
+	$('#gameOver').html('Well Done!');
+	$('#gameOver').css({
+		'background-color' : 'var(--tc)',
+		'color'            : 'var(--sc)',
+		'border'           : '2px solid var(--pc)',
+		'border-radius'    : '5px',
+		'padding'          : '20px',
+		'height'           : '10vh',
+		'font-family'      : 'var(--po)',
+		'font-size'        : '5vh',
+		'font-weight'      : 'bold',
+		'position'         : 'relative',
+		'top'              : '10vh'
+	});
+
+	// Add a 'Reset Game' button
+	$('<button id="gameReset"></button>').appendTo(
+		$('#contentInner')
+	);
+    $('#gameReset').html('Reset Game');
+    $('#gameReset').css({
+		'background-color' : 'var(--pc)',
+		'color'            : 'var(--sc)',
+		'border'           : '5px solid var(--tc)',
+		'border-radius'    : '5px',
+		'padding'          : '10px',
+		'font-family'      : 'var(--ebg)',
+		'font-size'        : '5vh',
+		'position'         : 'relative',
+		'top'              : '37.5vh'
+	});
 }
 
 // ---------------- Document Ready ---------------- //
 $(document).ready(function() {
-    // Give user the option to close alert if they've read the intro before
-    $('#closeBtn').click(closeBtn);
-    // Intro Alert - First click event
+	// Give user the option to close alert if they've read the intro before
+	$('#closeBtn').click(closeBtn);
+	// Intro Alert - First click event
 	$('#introBtn').click(intro1);
 });
